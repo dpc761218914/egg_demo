ac84f6d92ed02372d58a5d8935ab4ad73c4e6d7e
dpc761218914 <761218914@qq.com>
2018-07-15 21:56:59 +0800

    提交操作指南和搭建过程描述

--- app/common/responseMsg.js
+++ app/common/responseMsg.js
@@ -1,5 +1,6 @@
 module.exports = {
-  FORGET_PASSWORD: '忘记密码',
+  SUCCESS: '数据获取成功',
+  PASSWORD_ERROR: '密码错误',
   NEED_LOGIN: '用户未登录',
   NO_AUTH: '对不起，没有权限'
 };
--- app/controller/foreend/student.js
+++ app/controller/foreend/student.js
@@ -8,19 +8,31 @@ const Mock = require('mockjs');
 class StudentController extends Controller {
 
   async createStudent() {
+    const { ctx,service } = this;
     //1、获取用户post的参数
     //2、参数校验
     //3、调用service，存入数据
     //4、做响应与前端交互
+    const res={data:'this is data'};
+    const msg=ctx.response.ResponseMsg.SUCCESS;
     ctx.helper.success({ctx,msg,res});
   }
   async updateStudent() {
+    const { ctx,service } = this;
+    const res={data:'this is data'};
+    const msg=ctx.response.ResponseMsg.SUCCESS;
     ctx.helper.success({ctx,msg,res});
   }
   async getStudent() {
+    const { ctx,service } = this;
+    const res={data:'this is data'};
+    const msg=ctx.response.ResponseMsg.SUCCESS;
     ctx.helper.success({ctx,msg,res});
   }
   async delStudent() {
+    const { ctx,service } = this;
+    const res={data:'this is data'};
+    const msg=ctx.response.ResponseMsg.SUCCESS;
     ctx.helper.success({ctx,msg,res});
   }
   async getStudentList() {
@@ -31,7 +43,7 @@ class StudentController extends Controller {
     /* const res = await service.student.list();
      this.ctx.body = res;*/
     const res={data:'this is data'};
-    const msg=ctx.response.ResponseMsg.NO_AUTH;
+    const msg=ctx.response.ResponseMsg.PASSWORD_ERROR;
     if(1){
       //如果用户提交参数有误，需要返回异常的情况
       ctx.helper.fail({ctx,msg,res});
